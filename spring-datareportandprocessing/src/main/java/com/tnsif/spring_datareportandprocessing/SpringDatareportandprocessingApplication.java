7b9c58ed6d42c280f4936e553776746021381bb5
ADMIN <ADMIN@RAGAVIarunagiri>
2025-08-17 13:34:50 +0530

    Spring-DataReportandProcessing

---
+++ spring-datareportandprocessing/src/main/java/com/tnsif/spring_datareportandprocessing/SpringDatareportandprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_datareportandprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatareportandprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatareportandprocessingApplication.class, args);
+	}
+
+}
---
+++ spring-datareportandprocessing/src/test/java/com/tnsif/spring_datareportandprocessing/SpringDatareportandprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.spring_datareportandprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatareportandprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
